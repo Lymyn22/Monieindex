b3a541cb038316279793a2f206ddb8befbbc313a
Lymyn22 <alameenabdulrazaq2013@gmail.com>
2023-11-21 23:06:38 +0100

    my monieindex project

---
+++ src/components/Monieindex.jsx
@@ -0,0 +1,238 @@
+
+
+const Monieindex = ()=> {
+    return (
+       <div>
+        <div id="parent">
+            <div id="navigation">
+                <nav id="firstnav">
+                    <a href="" className="nav"><b>Business</b></a>
+                    <a href="" className="nav"><b>Personal</b></a>
+                </nav>
+            </div>
+
+            <div>
+                <nav id="secondnav">
+                    <a href="" className="nav"><b>About</b></a>
+                    <a href="" className="nav"><b>Career</b> &#x2197</a>
+                    <a href="" className="nav"><b>Contact Us</b></a>
+                    <div className="dropdown">
+                      <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      <img src="./assets/images/icons8-nigeria-circular-48.png" width="15px" height="15px" className="circle" /><span>NG</span>
+                      </button>
+                      <ul className="dropdown-menu">
+                        <li><button className="dropdown-item" type="button">Action</button></li>
+                        <li><button className="dropdown-item" type="button">Another action</button></li>
+                        <li><button className="dropdown-item" type="button">Something else here</button></li>
+                      </ul>
+                    </div>
+                </nav>
+            </div>
+    </div>
+
+    <div id="navigation2">
+        <div className="mnp1">
+          <a href="https:://moniepoint.com"><img src="./assets/images/Mop.1.webp" width="200px" height="55px" alt="" /></a>
+        </div>
+
+        <div id="navigation3">
+          <nav>
+            <a href=""  className="mnp2"><span>Banking</span></a>
+            <a href=""  className="mnp2"><span>Payments</span></a>
+            <a href=""  className="mnp2"><span>Credit</span></a>
+            <a href=""  className="mnp2"><span>Business Management</span></a>
+          </nav>
+        </div>
+
+        <div>
+          <button className="button"><b>Sign In</b></button>
+        </div>
+    </div>
+
+    <section id="backgroundvid">
+      <div className="myvideo"><video autoplay loop  muted play-inline   className="backvideo"><source src="./assets/video/hero-30387072ff43fcdb49eaa15cb2b4802c.mp4" type="video/mp4" /></video></div>
+
+      <div id="container">
+        <span className="body1">
+        <span className="edit2"><img src="./assets/images/download-tag.png" width="32" height="32" alt="" className="imageedit" /><span className="edit">National Inclusive Payment Initiative Award by CBN</span></span>
+        </span>
+        <div><h1>Simple solutions to power your business</h1></div>
+        <p className="para">Collect payments, access loans and manage operations with a business banking solution that meets all your needs.</p>
+        <button type="button" className="btn btn-primary btn-lg" id="button"><span>Open an account</span><span> &#x2192</span></button>
+      </div>
+    </section>
+
+    <section className="Thirdvw">
+      <div>
+        <h2>All the tools you need to run your business with ease</h2>
+      </div>
+      <div className="pics">
+        <div><img src="./assets/images/tools-2.webp" width="250" height="250"alt="" /><div className="point"><b>Point of Sale Terminals</b></div></div>
+        <div><img src="./assets/images/tools-3.webp" width="250" height="250"alt="" /><div className="expense"><b>Expense Cards</b></div></div>
+        <div><img src="./assets/images/tools-1.webp" width="250" height="250" alt="" /><div className="bus"><b>Business Account</b></div></div>
+        <div><img src="./assets/images/tools-4.webp" width="250" height="250" alt="" /><div className="working"><b>Working Capital Loans</b></div></div>
+        
+      </div>
+    </section>
+
+    <section>
+
+    </section>
+
+    <section>
+      <div className="container" id="flex1">
+        <div>
+          <h3>A free bank account to power your business</h3>
+          <span>Open an account for free</span>
+          <div></div>
+          <span>Get verified within a few minutes</span>
+          <div></div>
+          <span>Complete control over your account</span>
+          <div></div>
+          <span>Round-the-clock business banking support</span>
+          <div></div>
+          <span>Instant notifications</span>
+          <div></div>
+          <button type="button" className="btn btn-primary" data-bs-toggle="button">Get your account</button>
+        </div>
+        <img src="./assets/images/slide-1.webp" width="500px" height="600px" alt="" />
+      </div>
+    </section>
+
+    <section>
+      <div className="container" id="flex1">
+        <h4>POS machine wey no get wahala</h4>
+        <span></span>
+        <span>Secure payments that protect you and your customers</span>
+          <div></div>
+          <span>Easy-to-use operating system</span>
+          <div></div>
+          <span>Instant dispute resolution whenever you need it</span>
+          <div></div>
+          <span>Compatible with all cards</span>
+          <div></div>
+          <span>Long-lasting battery</span>
+          <div></div>
+          <span>Accepts card and transfer payments</span>
+          <div></div>
+          <button type="button" className="btn btn-primary" data-bs-toggle="button">Join the waitlist</button>
+        <div>
+        <img src="./assets/images/slide-2.webp" width="500px" height="600px" alt="" />
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div className="container" id="flex1">
+        <h4>Flexible loans that help your business grow</h4>
+        <span></span>
+        <span>Get up to N50,000,000 without collateral</span>
+          <div></div>
+          <span>Enjoy interest rates capped at 10% per annum</span>
+          <div></div>
+          <span>Repayment tenures that match your pace</span>
+          <div></div>
+          <span>Get loans approved in 3 days or less</span>
+          <button type="button" className="btn btn-primary" data-bs-toggle="button">Get an expense card</button>
+        <div>
+        <img src="./assets/images/tools-3.webp" width="500px" height="600px" alt="" />
+        </div>
+      </div>
+    </section>
+
+    <section className="viewport">
+      <div className="container">
+        <h4>A secure card for your business expenses</h4>
+        <span></span>
+        <span>Works with ATMs, POS terminals, and more</span>
+          <div></div>
+          <span>Start spending in as little as 48hrs</span>
+          <div></div>
+          <span>Set spending limits per expense card</span>
+          <div></div>
+          <span>Track what your business spends</span>
+          <div></div>
+          <span>High-level card security</span>
+          <div></div>
+          <button type="button" className="btn btn-primary" data-bs-toggle="button">Apply for a loan</button>
+        <div>
+        <img src="./assets/images/slide-4.webp" width="500px" height="600px" alt="" />
+        </div>
+      </div>
+    </section>
+
+    <section id="aliceback">
+      <div className="container">
+        <h4 className="aliceback">Join 600,000+ successful business owners who trust Moniepoint for all their business needs.</h4>
+        <img src="./assets/images/business-1.webp" width="146" height="93" alt=""  className="padding" />
+        <img src="./assets/images/business-2.webp" width="146.41" height="93" alt="" className="padding" />
+        <img src="./assets/images/business-3.webp" width="146" height="100" alt=""  className="padding" />
+        <img src="./assets/images/business-4.webp" width="146" height="77" alt=""  className="padding" />
+        <img src="./assets/images/business-5.webp" width="146" height="46" alt=""  className="padding" />
+      </div>
+    </section>
+
+    <section>
+    <div className="container">
+    <h4>Don't just take our word for it</h4>
+    <iframe width="1000" height="600" src="https://www.youtube.com/embed/dM3NmmXb6K4" title="Tobi Hamilton on Moniepoint - The House" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    </section>
+
+    <section className="firstfoot">
+      <div className="container" id="firstfoot">
+
+        <div>Connect with us</div>
+
+        <div className="footest">
+        <div>Head Office</div>
+        <div>Oyo-Ibadan Road, Idi-Agba, Ilora, Oyo</div>
+        </div>
+
+        <div className="footest">
+          <div>Company</div>
+          <div><a href="" className="txtfoot">About us</a></div>
+          <div><a href="" className="txtfoot">Join the team</a></div>
+          <div><a href="" className="txtfoot">Contact us</a></div>
+        </div>
+
+        <div className="footest">
+          <div>Our Products</div>
+          <div><a href="" className="txtfoot">Business Account</a></div>
+          <div><a href="" className="txtfoot">POS Terminals</a></div>
+          <div><a href="" className="txtfoot">Expense Cards</a></div>
+          <div><a href="" className="txtfoot">Working Capital Loans</a></div>
+          <div><a href="" className="txtfoot">Monnify</a></div>
+        </div>
+
+        <div className="footest">
+          <div>Resources</div>
+          <div><a href="" className="txtfoot">Help Centre</a></div>
+          <div><a href="" className="txtfoot">Learning Centregit </a></div>
+          <div><a href="" className="txtfoot">Whistleblower Policy</a></div>
+          <div><a href="" className="txtfoot">Privacy Policy</a></div>
+          <div><a href="" className="txtfoot">Cookie Policy</a></div>
+          <div><a href="" className="txtfoot">IMS Policy</a></div>
+        </div>
+
+        <hr />
+
+        
+      </div>
+    </section>
+    <section className=".firstfoot">
+      <div className="container" id="firstfoot">
+        <div></div>
+      <div>
+        <img src="./assets/images/MFB-white-540b83c3dcd8866ebc4d5c426dcabd78.webp" width="190px" height="55px" alt="" />
+      </div>
+      <div>Moniepoint Microfinance Bank Limited is authorised and regulated by the Central Bank of Nigeria to offer banking services. Deposits are insured by the Nigerian Deposit Insurance Corporation (NDIC).</div>
+      <div>Nigeria</div>
+    </div>
+    </section>
+
+       </div> 
+    )
+}
+
+export default Monieindex
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import './index.css'
+import './css/booststrapp.css'
+import Monieindex from './components/Monieindex'
+import './assets/images/icons8-nigeria-circular-48.png'
+import './assets/images/Mop.1.webp'
+import './assets/video/hero-30387072ff43fcdb49eaa15cb2b4802c.mp4'
+import './assets/images/download-tag.png'
+import './assets/images/tools-2.webp'
+import './assets/images/tools-3.webp'
+import './assets/images/tools-1.webp'
+import './assets/images/tools-4.webp'
+import './assets/images/slide-1.webp'
+import './assets/images/slide-2.webp'
+import './assets/images/tools-3.webp'
+import './assets/images/slide-4.webp'
+import './assets/images/business-1.webp'
+import './assets/images/business-2.webp'
+import './assets/images/business-3.webp'
+import './assets/images/business-4.webp'
+import './assets/images/business-5.webp'
+import './assets/images/MFB-white-540b83c3dcd8866ebc4d5c426dcabd78.webp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Monieindex />
+  </React.StrictMode>,
+)
